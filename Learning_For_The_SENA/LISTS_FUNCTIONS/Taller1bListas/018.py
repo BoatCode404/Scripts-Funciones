f5d3249db25f7ee3abc429005b8b8d7e413add5a
boatcode404 <boatcode404@gmail.com>
2024-07-24 14:53:38 -0500

    Taller1b

--- Learning_For_The_SENA/LISTS_FUNCTIONS/Taller1bListas/018.py
+++ Learning_For_The_SENA/LISTS_FUNCTIONS/Taller1bListas/018.py
@@ -1,3 +1,8 @@
 #Escribe un algoritmo que genere una lista de pares ordenados (tuplas) a partir de dos listas de igual longitud.
 
+lista1=[1,2,3]
+lista2=['a','b','c']
 
+unida=list(zip(lista1,lista2))
+
+print(unida)
